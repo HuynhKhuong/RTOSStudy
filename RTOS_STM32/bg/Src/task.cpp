2369ba6ea44c0ace9592c98743afb93f96e9ed5d
HuynhKhuong <thebestman2204@gmail.com>
2024-10-01 22:31:39 +0700

    cooperative scheduling, record, and observe

--- RTOS_STM32/bg/Src/task.cpp
+++ RTOS_STM32/bg/Src/task.cpp
@@ -4,24 +4,29 @@ namespace Task
 {
   TaskHandle_t taskHandle1;
   TaskHandle_t taskHandle2;
+  constexpr uint8_t g_msgLength{100U};
   
   static void task1Run(void* param)
   {
     //uint32_t tickTime_u32{100}; //100 tick rate = 100ms
+    char msg[g_msgLength]{0U};
     while(1)
     {
-      printf("Hello from task %d \n", 1U); 
-      //vTaskDelay(tickTime_u32);
+      snprintf(&msg[0U],g_msgLength,"Hello from task %d \n", 1U);
+      SEGGER_SYSVIEW_PrintfTarget(msg);
+      taskYIELD();
     }
   }
 
   static void task2Run(void* param)
   {
     //uint32_t tickTime_u32{100};
+    char msg[g_msgLength]{0U};
     while(1)
     {
-      printf("Hello from task %d \n", 2U); 
-      //vTaskDelay(tickTime_u32);
+      snprintf(&msg[0U],g_msgLength,"Hello from task %d \n", 2U);
+      SEGGER_SYSVIEW_PrintfTarget(msg);
+      taskYIELD();
     }
   }
 
