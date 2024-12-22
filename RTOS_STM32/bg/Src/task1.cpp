3a5408be709e1372fe54e2374c2b9657fa82403e
HuynhKhuong <thebestman2204@gmail.com>
2024-12-22 19:21:47 +0700

    implement notification from task4 to task1

--- RTOS_STM32/bg/Src/task1.cpp
+++ RTOS_STM32/bg/Src/task1.cpp
@@ -7,12 +7,23 @@ namespace Task
   void Task1Handler::task1Run(void* param)
   {
     task1.m_currentWakeTimeTick = xTaskGetTickCount();
+    uint32_t ulNotificationValue{0U};
 
     while(1)
     {
       //User code to do here
-      task1.m_LEDHandler.blinkLED();
-      vTaskDelayUntil(&task1.m_currentWakeTimeTick, task1.m_taskCycleTick);
+      //
+
+      xTaskNotifyWait( 0U, 0x1U, &ulNotificationValue, 0U);
+      if(ulNotificationValue == 1)
+      {
+        vTaskDelete(task1.getTaskHandle());
+      }
+      else
+      {
+        task1.m_LEDHandler.blinkLED();
+        vTaskDelayUntil(&task1.m_currentWakeTimeTick, task1.m_taskCycleTick);
+      }
     }
   }
 
--- RTOS_STM32/bg/Src/task4.cpp
+++ RTOS_STM32/bg/Src/task4.cpp
@@ -1,8 +1,12 @@
 #include "task4.hpp"
+#include "task1.hpp"
 
 namespace Task
 {
   Task4Handler task4{};  //to define task4 here
+  /*
+   * Example for sending notification to task 1
+   */
 
   void Task4Handler::task4Run(void* param)
   {
@@ -12,6 +16,11 @@ namespace Task
     {
       //User code to do here
       task4.m_userInputHandler.queryInputFromHardware();
+      if(task4.m_userInputHandler.isInputClicked())
+      {
+        xTaskNotify(task1.getTaskHandle(), 0x1U, eNotifyAction::eSetBits);
+      }
+
       vTaskDelayUntil(&task4.m_currentWakeTimeTick, task4.m_taskCycleTick);
     }
   }
