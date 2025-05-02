63dd88e534dde0605edf95ea5298df3aec8f0b24
HuynhKhuong <thebestman2204@gmail.com>
2025-05-02 10:04:34 +0700

    fix runnable operating sequence

--- RTOS_STM32/apl/foundation/runnable/RunnableDefine.hpp
+++ RTOS_STM32/apl/foundation/runnable/RunnableDefine.hpp
@@ -8,16 +8,26 @@ public:
   RunnableInf() = default;
   void run()
   {
-    m_txCustomerHook();
+	m_rxCustomerHook();
     m_customerRun();
-    m_rxCustomerHook();
+    m_txCustomerHook();
   }
 
   virtual void init() = 0;
 
 protected:
+  ///\brief from runnable perspective, rxCustomerHook provides user intervention
+  ///		to handle incoming messages before processing its services
+  ///\note	as its design purpose, it must be called before run
   virtual void m_rxCustomerHook(void) = 0;
+
+  ///\brief from runnable perspective, txCustomerHook provides user intervention
+  ///		to handle its outputs before after processing its services
+  ///\note	as its design purpose, it must be called after run
   virtual void m_txCustomerHook(void) = 0;
+
+  ///\brief main function for runnable to process its services
+  ///\note	as its design purpose, it must be called cyclically
   virtual void m_customerRun(void) = 0;
 };
 
