638c672123a01f3854dc86f830345409b9c408f7
HuynhKhuong <thebestman2204@gmail.com>
2025-07-06 21:22:41 +0700

    - Added main state ID encoding for RTC and LED menu signals by combining state and value in a single signal. - Updated RTC control notification to send both menu and RTC config signals

--- RTOS_STM32/apl/notify/ComButInputNotify.cpp
+++ RTOS_STM32/apl/notify/ComButInputNotify.cpp
@@ -1,8 +1,14 @@
 #include "ComBusInputNotify.hpp"
+#include "MenuStateMachine.hpp"
 #include "SymbolicNames.hpp"
 #include "ComReceive.hpp"
 #include "net/NetPortIntf.hpp"
 #include "net/NetSenderPort.hpp"
+
+namespace
+{
+  constexpr uint32_t mainStateIdOffset_cu32{24U};
+}
 
 namespace NetCom 
 {
@@ -11,49 +17,57 @@ namespace NetCom
     uint8_t signalVal_u8{0U}; 
     netComReceiveSignal(g_LEDModeReqSignal_u8, static_cast<void*>(&signalVal_u8));  
     uint32_t& reservedChunk = Port::g_netMenuSenderPort_st.reserve();
-    reservedChunk = signalVal_u8;
+
+    ///\brief  main state id and the signal value is stored into the same signal as for memory optimization
+    reservedChunk = (static_cast<uint32_t>(Menu::MainStateIdList::LedMenu) << mainStateIdOffset_cu32) | static_cast<uint32_t>(signalVal_u8);
   }
 
   void s_RTCControlMsgNotif_Rx(void)
   {
-    if(Port::g_netMenuRtcConfigSenderPort_st.isConnected())
+    if((Port::g_netMenuRtcConfigSenderPort_st.isConnected() == false) || 
+        (Port::g_netMenuSenderPort_st.isConnected() == false))
     {
-      Port::RTCReqInf& reservedChunk = Port::g_netMenuRtcConfigSenderPort_st.reserve();
-			{
-				uint8_t signalVal_u8{0U}; 
-				netComReceiveSignal(g_RTCModeReqSignal_u8, static_cast<void*>(&signalVal_u8));
-	      reservedChunk.m_RTCRequest.setSignalVal(signalVal_u8);
-			}
-			{
-				uint8_t signalVal_u8{0U}; 
-				netComReceiveSignal(g_ReqDateSignal_u8, static_cast<void*>(&signalVal_u8));
-	      reservedChunk.m_day.setRawSignalVal(signalVal_u8);
-			}
-			{
-				uint8_t signalVal_u8{0U}; 
-				netComReceiveSignal(g_ReqMonthSignal_u8, static_cast<void*>(&signalVal_u8));
-	      reservedChunk.m_month.setRawSignalVal(signalVal_u8);
-			}
-			{
-				uint16_t signalVal_u16{0U}; 
-				netComReceiveSignal(g_ReqYearSignal_u16, static_cast<void*>(&signalVal_u16));
-	      reservedChunk.m_year.setRawSignalVal(signalVal_u16);
-			}
-			{
-				uint8_t signalVal_u8{0U}; 
-				netComReceiveSignal(g_ReqSecondsSignal_u8, static_cast<void*>(&signalVal_u8));
-	      reservedChunk.m_second.setRawSignalVal(signalVal_u8);
-			}
-			{
-				uint8_t signalVal_u8{0U}; 
-				netComReceiveSignal(g_ReqMinutesSignal_u8, static_cast<void*>(&signalVal_u8));
-	      reservedChunk.m_minute.setRawSignalVal(signalVal_u8);
-			}
-			{
-				uint8_t signalVal_u8{0U}; 
-				netComReceiveSignal(g_HoursSignal_u8, static_cast<void*>(&signalVal_u8));
-	      reservedChunk.m_hour.setRawSignalVal(signalVal_u8);
-			}
+      return;
+    }
+
+    Port::RTCReqInf& reservedChunk_st = Port::g_netMenuRtcConfigSenderPort_st.reserve();
+    uint32_t& reservedChunk_u32 = Port::g_netMenuSenderPort_st.reserve();
+
+    reservedChunk_u32 = static_cast<uint32_t>(Menu::MainStateIdList::RTCMenu) << mainStateIdOffset_cu32;
+    {
+      uint8_t signalVal_u8{0U}; 
+      netComReceiveSignal(g_RTCModeReqSignal_u8, static_cast<void*>(&signalVal_u8));
+      reservedChunk_st.m_RTCRequest.setSignalVal(signalVal_u8);
+    }
+    {
+      uint8_t signalVal_u8{0U}; 
+      netComReceiveSignal(g_ReqDateSignal_u8, static_cast<void*>(&signalVal_u8));
+      reservedChunk_st.m_day.setRawSignalVal(signalVal_u8);
+    }
+    {
+      uint8_t signalVal_u8{0U}; 
+      netComReceiveSignal(g_ReqMonthSignal_u8, static_cast<void*>(&signalVal_u8));
+      reservedChunk_st.m_month.setSignalVal(signalVal_u8);
+    }
+    {
+      uint16_t signalVal_u16{0U}; 
+      netComReceiveSignal(g_ReqYearSignal_u16, static_cast<void*>(&signalVal_u16));
+      reservedChunk_st.m_year.setSignalVal(signalVal_u16);
+    }
+    {
+      uint8_t signalVal_u8{0U}; 
+      netComReceiveSignal(g_ReqSecondsSignal_u8, static_cast<void*>(&signalVal_u8));
+      reservedChunk_st.m_second.setSignalVal(signalVal_u8);
+    }
+    {
+      uint8_t signalVal_u8{0U}; 
+      netComReceiveSignal(g_ReqMinutesSignal_u8, static_cast<void*>(&signalVal_u8));
+      reservedChunk_st.m_minute.setSignalVal(signalVal_u8);
+    }
+    {
+      uint8_t signalVal_u8{0U}; 
+      netComReceiveSignal(g_HoursSignal_u8, static_cast<void*>(&signalVal_u8));
+      reservedChunk_st.m_hour.setSignalVal(signalVal_u8);
     }
   }
 }
