75adf7ca6118d734792de819ddbc0cfe8160e24b
HuynhKhuong <thebestman2204@gmail.com>
2025-08-23 18:54:58 +0700

    update led mode value table

--- RTOS_STM32/apl/led/LedModeManager.cpp
+++ RTOS_STM32/apl/led/LedModeManager.cpp
@@ -16,16 +16,16 @@ namespace Led
     /* Strategy pattern + factory method for choosing the correct mode */
     switch(index)
     {
-        case 1U:
+        case 0U:
           m_currentLedMode = static_cast<LedModeInf*>(&LedModeAllBlink::getSingleton());
           break;
-        case 2U:
+        case 1U:
           m_currentLedMode = static_cast<LedModeInf*>(&LedModePairBlink1::getSingleton());
           break;
-        case 3U:
+        case 2U:
           m_currentLedMode = static_cast<LedModeInf*>(&LedModePairBlink2::getSingleton());
           break;
-        case 4U:
+        case 3U:
           m_currentLedMode = static_cast<LedModeInf*>(&LedModeBlinkCircle::getSingleton());
           break;
         default:
